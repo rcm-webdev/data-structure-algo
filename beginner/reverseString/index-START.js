b63016a352d96f410b9894a89f3455a93f143275
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-07 13:36:36 -0800

    update method with spread operator

--- beginner/reverseString/index-START.js
+++ beginner/reverseString/index-START.js
@@ -1,7 +1,8 @@
 //Given a string of text, write an algorithm that returns the text received in a reversed format. E.g
 
 function reverseString(text) {
-  return text.split("").reverse().join("");
+  //... spread opertor and split can do the same thing
+  return [...text].reverse().join("");
 }
 
 module.exports = reverseString;

0c68eab0595304f1173c4f35a2fb874d52154594
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-07 18:37:23 -0800

    add for loop solution

--- beginner/reverseString/index-SOLUTION.js
+++ beginner/reverseString/index-SOLUTION.js
@@ -6,6 +6,18 @@ function reverseString(str) {
 
 //or with spread operator
 
-function reverseStringTwo(str) {
+function reverseStringSpread(str) {
   return [...str].reverse().join("");
+}
+
+//the spread operator [...] like the split method .split() will spread the characters of the string into array elements.
+
+//take a str
+//loop through a str? with a for loop
+function reverseStringForLoop(str) {
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
 }
